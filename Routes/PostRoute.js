a5554b1508e732c71bfe236b72f9dff69bf2dba0
chao lobi <gogoiladitya@gmail.com>
2023-07-29 20:48:37 +0530

    like/dislike logic implemented in the backend

--- Routes/PostRoute.js
+++ Routes/PostRoute.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { createPost, getPost } from '../Controllers/PostController.js';
+import { createPost, getPost, likePost } from '../Controllers/PostController.js';
 
 const router = express.Router();
 
 router.post('/upload', createPost);
 router.get('/:id', getPost);
+router.put("/:id/like", likePost);
 export default router;
